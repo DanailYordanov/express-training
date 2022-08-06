b09877810ee23aafd7182c74edfe54ff1ff13178
Danny <danail.yordanov1@gmail.com>
2022-08-06 16:57:16 +0300

    Add mongoose

--- hello-world-express/index.js
+++ hello-world-express/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, PORT } = require('./config/config');
 const app = express();
-const port = process.env.PORT || 3000;
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+    mongoose
+        .connect(mongoURL)
+        .then(() => console.log("Succesfully connected to MongoDB"))
+        .catch((e) => {
+            console.log(e);
+            setTimeout(connectWithRetry, 5000);
+        });
+}
+
+connectWithRetry();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
 })
